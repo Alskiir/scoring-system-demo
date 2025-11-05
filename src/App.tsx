1f654147f433a85e4a87828eef86505d1c87f6d1
Alskiir <Doughart215@gmail.com>
2025-11-05 18:11:59 -0500

    Add Navbar and set up React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
-import { ToggleSwitch } from "./components";
-import { useState } from "react";
+import { Navbar } from "./components";
 
 function App() {
-	const [isChecked, setIsChecked] = useState(true);
-
 	return (
-		<div className="min-h-screen flex flex-col items-center justify-center bg-white text-gray-900 transition-colors">
-			<h1 className="text-2xl font-bold mb-6">WPPL Demo App</h1>
-			<ToggleSwitch
-				checked={isChecked}
-				onChange={(checked) => {
-					console.log(checked);
-					setIsChecked(checked);
-				}}
-				label="Toggle Test"
-			/>
+		<div className="min-h-screen flex flex-col">
+			<Navbar />
+			<main className="flex-1 flex items-center justify-center">
+				<h1 className="text-2xl font-semibold">
+					Welcome to the WPPL Demo!
+				</h1>
+			</main>
 		</div>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
 import "./styles/global.css";
-import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />} />
+			</Routes>
+		</BrowserRouter>
 	</StrictMode>
 );
